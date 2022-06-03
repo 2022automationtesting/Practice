bca9f508b6f3ad149d707e520d234f13dbcd13ec
DELL <2022automationtesting@gmail.com>
2022-06-03 20:02:22 +0530

    Demo1

---
+++ src/com/WorkingDemo/Demo1.java
@@ -0,0 +1,9 @@
+package com.WorkingDemo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+    }
+}
